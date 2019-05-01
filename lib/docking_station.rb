a413f384737879494561cb93938458fa03d41725
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-05-01 22:27:22 +0100

    Dock and track bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,15 @@
 class DockingStation
+  attr_reader :bikes
+
+  def initialize(bikes)
+    @bikes = bikes
+  end
+
   def release_bike
     Bike.new
+  end
+
+  def dock_bike(bike)
+    @bikes.push(bike)
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,7 +4,7 @@ describe DockingStation do
 
   # reset state before each test
   before :each do
-    @docking_station = DockingStation.new
+    @docking_station = DockingStation.new([])
   end
 
    it 'has a release_bike method' do
@@ -14,4 +14,30 @@ describe DockingStation do
    it 'can release a bike' do
      expect(@docking_station.release_bike).to be_an_instance_of(Bike)
    end
+
+   it 'has a dock_bike method' do
+     expect(@docking_station).to respond_to(:dock_bike)
+   end
+
+   it 'can dock a bike' do
+     bike = Bike.new
+     @docking_station.dock_bike(bike)
+     expect(@docking_station.bikes.length).to eq(1)
+     expect(@docking_station.bikes[0]).to eq bike
+   end
+
+   it 'requires an argument when initialized' do
+     expect(DockingStation).to respond_to(:new).with(1).argument
+   end
+
+   it 'can be initialized with bikes' do
+     bike_1 = Bike.new
+     bike_2 = Bike.new
+     docking_station = DockingStation.new([bike_1, bike_2])
+
+     expect(docking_station.bikes.length).to eq(2)
+     expect(docking_station.bikes[0]).to eq(bike_1)
+     expect(docking_station.bikes[1]).to eq(bike_2)
+   end
+
 end
