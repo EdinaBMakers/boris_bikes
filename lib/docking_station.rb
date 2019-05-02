572391b5cc23c53ed9079d52d673b6147d58c362
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-05-02 20:22:53 +0100

    Refactor guard conditions

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,12 +7,22 @@ class DockingStation
   end
 
   def release
-    raise RuntimeError, "No bike in the docking station" if @bikes.empty?
+    raise RuntimeError, "No bike in the docking station" if empty?
     @bikes.first
   end
 
   def dock(bike)
-    raise RuntimeError, "Docking station is full" if @bikes.size == @capacity
+    raise RuntimeError, "Docking station is full" if full?
     @bikes.push(bike)
+  end
+
+  private
+
+  def full?
+    @bikes.size >= @capacity
+  end
+
+  def empty?
+    @bikes.empty?
   end
 end
